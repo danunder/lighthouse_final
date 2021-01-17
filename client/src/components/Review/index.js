3189aa253534a2336a2934c81b34fcdeaf553dc9
emtupp <emilyc.tupper@gmail.com>
2021-01-17 21:48:46 +0000

    add some placeholder modes for rendering the components

--- client/src/components/Review/index.js
+++ client/src/components/Review/index.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ReviewForm from "./ReviewForm"
 import "./styles.css";
 
+// taken from Scheduler project, needs to be modified to fit this project, and tucked into helper file
+
+function useVisualMode(initial) {
+  
+  const [ mode, setMode ] = useState(initial);
+  const [ history, setHistory ] = useState([initial]);
+
+  // Creates an array with the states' history
+  const transition = (changeMode, replace = false) => {
+    if (!replace) {
+      setHistory(prev => [...prev, changeMode]);
+    } else {
+      setHistory(prev => {
+        prev.pop();
+        return [...prev, changeMode]
+      });
+    };
+    setMode(changeMode);
+  };
+  
+  const back = () => {
+    setHistory(prev => {
+      if (history.length > 1) {
+        prev.pop();
+        setMode(prev[prev.length -1])
+      } else {
+        setMode(initial);
+      };
+      return prev;
+    });
+  };
+
+  return { mode, transition, back };
+};
+
+// ends here
+
 export default function Review(props) {
 
-  const WRITE = "WRITE";
+  const REVIEW1 = "REVIEW1";
 
-  return <section classname="review" data-testid="review">
+  // scheduler code
+  const { mode } = useVisualMode();
+
+  return <article classname="review" data-testid="review">
+    {mode === REVIEW1 && <ReviewForm />}
     <ReviewForm />
-  </section>
+  </article>
 };
