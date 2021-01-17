94bed0453cff69f7391c42c0dbc718da7db0efb5
emtupp <emilyc.tupper@gmail.com>
2021-01-17 20:55:33 +0000

    add hovering effect over stars

--- client/src/components/StarRating.js
+++ client/src/components/StarRating.js
@@ -7,6 +7,7 @@ import "./StarRating.css";
 
 export default function StarRating(props) {
   const [ rating, setRating ] = useState(null);
+  const [ hover, setHover ] = useState(null);
 
   return (
   <div>
@@ -16,12 +17,15 @@ export default function StarRating(props) {
         <input
           type='radio'
           name='rating'
-          value={ratingValue} onClick={() => setRating(ratingValue)}
+          value={ratingValue}
+          onClick={() => setRating(ratingValue)}
         />
         <FaStar
           className='star'
-          color={ratingValue <= rating ? "gold" : "rgba(128, 128, 128, 0.246)"}
+          color={ratingValue <= (hover || rating) ? "gold" : "rgba(128, 128, 128, 0.246)"}
           size={50}
+          onMouseEnter={() => setHover(ratingValue)}
+          onMouseLeave={() => setHover(null)}
         />
       </label>;
     })}
