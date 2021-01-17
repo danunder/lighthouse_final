064b4174637a442f1260be3cdfd6bd0779e30f0c
emtupp <emilyc.tupper@gmail.com>
2021-01-17 22:06:36 +0000

    prepare branch for merge

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 import Map from './components/Map';
+// Review component not yet apprearing on page
 import Review from './components/Review';
 
 function App() {
   return (
     <div className='App'>
-      <Review />
-      {/* <h1>Google Maps App</h1>
-      <Map /> */}
+      <h1>Google Maps App</h1>
+      <Map />
     </div>
   );
 }
