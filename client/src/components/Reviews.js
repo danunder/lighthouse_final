84105a4281de4265b76cda695346cde15e542038
Jared Flomen <jaredflomen@gmail.com>
2021-01-17 16:05:23 -0500

    Another style for the cards

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import AddReview from './AddReview';
+// import AddReview from './AddReview';
+import { Card } from 'react-bootstrap';
 
 export default function Reviews() {
   const fakeReview = [
@@ -20,20 +21,30 @@ export default function Reviews() {
       stars: 5,
     },
   ];
-  const mapData = fakeReview.map(data => {
-    return (
-      <div className='review-block'>
-        <h1>*****</h1>
-        <h1>{data.name}</h1>
-        <p>{data.stars} stars</p>
-        <p>View More ...</p>
-      </div>
-    );
-  });
+  // const mapData = fakeReview.map(data => {
+  //   return (
+  //     <div className='review-block'>
+  //       <h1>*****</h1>
+  //       <h1>{data.name}</h1>
+  //       <p>{data.stars} stars</p>
+  //       <p>View More ...</p>
+  //     </div>
+  //   );
+  // });
   return (
-    <>
-      {mapData}
-      <AddReview />
-    </>
+    // <>
+    //   {mapData}
+    //   <AddReview />
+    // </>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Jared F</Card.Title>
+        <Card.Subtitle className='mb-2 text-muted'>5 Stars</Card.Subtitle>
+        <Card.Text>
+          Here we would input a snipped from the user's review of the property.
+        </Card.Text>
+        <Card.Link href='#'>View More</Card.Link>
+      </Card.Body>
+    </Card>
   );
 }
