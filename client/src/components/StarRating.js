a8e0aac1b0f9931cac67dbce1b91c606274ed370
emtupp <emilyc.tupper@gmail.com>
2021-01-17 20:50:47 +0000

    create StarRating component and its CSS file

---
+++ client/src/components/StarRating.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+import { FaStar } from 'react-icons/fa'
+
+import "./StarRating.css";
+
+
+export default function StarRating(props) {
+  const [ rating, setRating ] = useState(null);
+
+  return (
+  <div>
+    {[ ... Array(5)].map((star, i) => {
+      const ratingValue = i + 1
+      return <label>
+        <input
+          type='radio'
+          name='rating'
+          value={ratingValue} onClick={() => setRating(ratingValue)}
+        />
+        <FaStar
+          className='star'
+          color={ratingValue <= rating ? "gold" : "rgba(128, 128, 128, 0.246)"}
+          size={50}
+        />
+      </label>;
+    })}
+  </div>
+  );
+};
