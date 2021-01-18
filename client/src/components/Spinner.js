57813dee95437f0936e19b1b92df24a5c4b16a76
emtupp <emilyc.tupper@gmail.com>
2021-01-18 18:51:15 +0000

    work on processing component and fix class to become className in other components

---
+++ client/src/components/Spinner.js
@@ -0,0 +1,10 @@
+import { FaStar } from 'react-icons/fa';
+import './Spinner.css'
+
+export default function Spinner(props) {
+  return (
+    <section className='spinner'>
+      <div className="spinner-grow"></div>
+    </section>
+  );
+};
