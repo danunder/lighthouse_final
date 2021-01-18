2c136683b3ef916fffb653c64a9ba96f01b94dd5
Jared Flomen <jaredflomen@gmail.com>
2021-01-18 09:35:33 -0500

    Combining the fake data with the better looking review card

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -8,47 +8,43 @@ export default function Reviews() {
     {
       name: 'Jared',
       stars: 4,
+      review: 'So good',
     },
     {
       name: 'Jimmy',
       stars: 2,
+      review: 'So bad',
     },
     {
       name: 'Dan',
       stars: 3,
+      review: 'I love this hood!',
     },
     {
       name: 'Emily',
       stars: 5,
+      review: 'I am going to live here forever!',
     },
   ];
   const mapData = fakeReview.map(data => {
     return (
-      <div className='review-block'>
-        {/* <h1>*****</h1> */}
-        <FaStar />
-        <FaStar />
-        <FaStar />
-        <h1>{data.name}</h1>
-        <p>View More ...</p>
-      </div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{data.name}</Card.Title>
+          {/* <Card.Subtitle className='mb-2 text-muted'>5 Stars</Card.Subtitle> */}
+          {[...Array(data.stars)].map((e, index) => (
+            <FaStar />
+          ))}
+          <Card.Text>{data.review}</Card.Text>
+          <Card.Link href='#'>View More</Card.Link>
+        </Card.Body>
+      </Card>
     );
   });
   return (
     <>
-      {/* {mapData} */}
+      {mapData}
       {/* <AddReview /> */}
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>Jared F</Card.Title>
-          <Card.Subtitle className='mb-2 text-muted'>5 Stars</Card.Subtitle>
-          <Card.Text>
-            Here we would input a snipped from the user's review of the
-            property.
-          </Card.Text>
-          <Card.Link href='#'>View More</Card.Link>
-        </Card.Body>
-      </Card>
     </>
   );
 }
