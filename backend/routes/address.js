74ec9bb2e20023eb36b483cfece1d0f6c5b2b778
Jared <jaredflomen@gmail.com>
2021-01-19 21:07:53 +0000

    Basic user auth

--- backend/routes/address.js
+++ backend/routes/address.js
@@ -10,12 +10,12 @@ module.exports = ({ getReviews, login }) => {
       .catch(e => console.log('Backend error', e.message));
   });
   router.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const userAuth = login(username, password);
-    if (userAuth) {
-      req.session['user_id'] = res.rows[0].id;
-      data => res.json(data);
-    }
+    const { username, password } = req.body.user;
+    console.log('username ', username);
+    console.log('password', password);
+    login(username, password)
+      .then(data => res.json(data))
+      .catch(e => console.log('Backend error ', e.message));
   });
   return router;
 };
