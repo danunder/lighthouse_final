c58197140b0e80e9b6db61c04b236658c59b029e
Jared <jaredflomen@gmail.com>
2021-01-19 13:52:32 +0000

    API work

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaMapSigns } from 'react-icons/fa';
 import './App.css';
 import Map, { MapContainer } from './components/Map';
 import Test from './components/Test';
+import axios from 'axios';
 
 // import Reviews from './components/Reviews';
-// // import ReviewInput from './components/ReviewInput';
-// // Review component not yet apprearing on page
+// import ReviewInput from './components/ReviewInput';
+// Review component not yet apprearing on page
 // import Review from './components/Review';
 
 function App() {
-
   const [place, setPlace] = useState({});
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: '/api/',
+    }).then(({ data }) => {
+      console.log(data);
+    });
+  }, []);
 
   return (
     <div className='App'>
-      <Map
-        onSelect={setPlace}>
-
-      </Map>
-      <Test address={place.address ? place.address : 'No address has been selected'} />
+      <Map onSelect={setPlace}></Map>
+      <Test
+        address={place.address ? place.address : 'No address has been selected'}
+      />
     </div>
   );
 }
