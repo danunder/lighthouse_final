c90f8aff6e3a777bfbe9396eec33526aefeac639
Jared <jaredflomen@gmail.com>
2021-01-19 14:16:52 +0000

    API route with lat lng

--- backend/helpers/dbHelpers.js
+++ backend/helpers/dbHelpers.js
@@ -1,6 +1,6 @@
 module.exports = db => {
   // eslint-disable-next-line no-unused-vars
-  const getReviews = address => {
+  const getReviews = (lng, lat) => {
     const query = {
       text: `SELECT reviews.review, reviews.rating, users.username
       FROM users
@@ -8,9 +8,10 @@ module.exports = db => {
       JOIN reviews ON tenancies.id = reviews.tenancy_id
       JOIN properties ON reviews.property_id = properties.id
       JOIN categories ON reviews.category_id = categories.id
-      WHERE properties.place_id = $1
+      WHERE properties.longitude = $1
+      AND properties.latitude = $2
       AND categories.id = 1`,
-      values: [address],
+      values: [lat, lng],
     };
 
     return db
