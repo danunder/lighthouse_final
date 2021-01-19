41aa7c46f04cb0b2be68289ee3113fca6e8a51c3
Jared <jaredflomen@gmail.com>
2021-01-19 00:11:56 +0000

    cleaning up backend

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
 const address = require('./routes/address');
+
 const app = express();
 
 const db = require('./db');
 const dbHelpers = require('./helpers/dbHelpers')(db);
-app.use('/api/', address(dbHelpers));
-
+// console.log(dbHelpers);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/', address(dbHelpers));
 
 module.exports = app;
--- backend/routes/address.js
+++ backend/routes/address.js
@@ -4,9 +4,9 @@ const router = express.Router();
 module.exports = ({ getReviews }) => {
   router.get('/', (req, res) => {
     //Find a way to get lat and lng from url request
-    const { lat, lng } = req.body;
-    getReviews(lat, lng)
+    getReviews('ChIJd5Zc4zLL1IkRmG90lQIu_X4')
       .then(data => res.json(data))
       .catch(e => console.log('Backend error', e.message));
   });
+  return router;
 };
