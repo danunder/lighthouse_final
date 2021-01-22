ddf8db902d40e7a16802763bf89102b1b27acac0
emtupp <emilyc.tupper@gmail.com>
2021-01-22 01:59:24 +0000

    fix the props inside review cards

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -5,27 +5,37 @@ import { Card } from 'react-bootstrap';
 
 export default function Reviews(props) {
   const reviewData = props.data;
+  
+  const findPropertyReviews = () => {
+    const propertyReviews = [];
+    for (let review of reviewData) {
+      if (review.category_id === 1) {
+        propertyReviews.push(review)
+      }
+    }
+    return propertyReviews
+  };
 
-  // const mapData = reviewData.map(data => {
-  //   return (
-  //     <Card style={{ width: '18rem' }}>
-  //       <Card.Body>
-  //         <Card.Title>{data.username}</Card.Title>
-  //         {/* <Card.Subtitle className='mb-2 text-muted'>5 Stars</Card.Subtitle> */}
-  //         {[...Array(data.rating)].map((e, index) => (
-  //           <FaStar />
-  //         ))}
-  //         <Card.Text>{data.review}</Card.Text>
-  //         <Card.Link href='#' onClick={props.onClick}>
-  //           View More
-  //         </Card.Link>
-  //       </Card.Body>
-  //     </Card>
-  //   );
-  // });
+  const mapData = (findPropertyReviews()).map(data => {
+    return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{data.user}</Card.Title>
+          {/* <Card.Subtitle className='mb-2 text-muted'>5 Stars</Card.Subtitle> */}
+          {[...Array(data.rating)].map((e, index) => (
+            <FaStar />
+          ))}
+          <Card.Text>{data.review}</Card.Text>
+          <Card.Link href='#' onClick={props.onClick}>
+            View More
+          </Card.Link>
+        </Card.Body>
+      </Card>
+    );
+  });
   return (
     <>
-      {/* {mapData} */}
+      {mapData}
       <AddReview onClick={props.addNew} />
     </>
   );
