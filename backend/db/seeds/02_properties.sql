e9f79e418b77a3c01db3366e632084af66cbd6a4
Jared Flomen <jaredflomen@gmail.com>
2021-01-27 16:41:47 -0500

    Styling

--- backend/db/seeds/02_properties.sql
+++ backend/db/seeds/02_properties.sql
@@ -1,6 +1,4 @@
 INSERT INTO properties(
   place_id, latitude, longitude
 )
-VALUES(
-  'ChIJd5Zc4zLL1IkRmG90lQIu_X4', 43.65093, -79.37826
-)
+VALUES('ChIJd5Zc4zLL1IkRmG90lQIu_X4', 43.65093, -79.37826)
--- backend/db/seeds/03_tenancies.sql
+++ backend/db/seeds/03_tenancies.sql
@@ -2,7 +2,7 @@ INSERT INTO tenancies(start_date, end_date, user_id, property_id)
 VALUES('x', 'x', 1, 1);
 
 INSERT INTO tenancies(start_date, end_date, user_id, property_id)
-VALUES('x', 'x', 1, 1);
+VALUES('x', 'x', 3, 1);
 
 INSERT INTO tenancies(start_date, end_date, user_id, property_id)
 VALUES('x', 'x', 2, 1);
