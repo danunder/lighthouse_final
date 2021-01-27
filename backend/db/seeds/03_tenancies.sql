d9f895f387379f8b291856df2a97b799399d97e8
emtupp <emilyc.tupper@gmail.com>
2021-01-27 21:34:44 +0000

    reseed database and fix zindex of search bar

--- backend/db/seeds/03_tenancies.sql
+++ backend/db/seeds/03_tenancies.sql
@@ -1,8 +1,26 @@
 INSERT INTO tenancies(start_date, end_date, user_id, property_id)
-VALUES('x', 'x', 1, 1);
+VALUES('x', 'x', 4, 1);
 
 INSERT INTO tenancies(start_date, end_date, user_id, property_id)
-VALUES('x', 'x', 1, 1);
+VALUES('x', 'x', 5, 1);
 
 INSERT INTO tenancies(start_date, end_date, user_id, property_id)
-VALUES('x', 'x', 2, 1);
+VALUES('x', 'x', 6, 2);
+
+INSERT INTO tenancies(start_date, end_date, user_id, property_id)
+VALUES('x', 'x', 7, 2);
+
+INSERT INTO tenancies(start_date, end_date, user_id, property_id)
+VALUES('x', 'x', 8, 1);
+
+INSERT INTO tenancies(start_date, end_date, user_id, property_id)
+VALUES('x', 'x', 9, 2);
+
+INSERT INTO tenancies(start_date, end_date, user_id, property_id)
+VALUES('x', 'x', 10, 2);
+
+INSERT INTO tenancies(start_date, end_date, user_id, property_id)
+VALUES('x', 'x', 11, 2);
+
+INSERT INTO tenancies(start_date, end_date, user_id, property_id)
+VALUES('x', 'x', 12, 2);
--- backend/db/seeds/05_reviews.sql
+++ backend/db/seeds/05_reviews.sql
@@ -1,17 +1,88 @@
 INSERT INTO reviews(review, rating, category_id, tenancy_id)
-VALUES('The property is amazing, I loved living there!', 5, 1, 1);
+VALUES('J’ai demandé de briser mon bail après seulement un mois. Le logement avait des puces de lit!! J’ai perdu tous mes meubles. NE VENEZ PAS VIVRE ICI!', 1, 1, 1);
 
 INSERT INTO reviews(review, rating, category_id, tenancy_id)
-VALUES('The property is a piece of garbage, a huge mistake living there!', 1, 1, 2);
+VALUES('Le Plateau Mont Royal est vraiment le meilleur quartier sur l Île. J aime me balader et visiter les boutiques et restaurants pendant mes weekend. Le Mont-Royal est à distance de marche. Magnifique.', 5, 2, 1);
 
 INSERT INTO reviews(review, rating, category_id, tenancy_id)
-VALUES('The neighbourhood has some fantastic shops to explore!', 4, 2, 1);
+VALUES('Il était gentil et tout semblait bien, mais je me doute qu’il était au courant pour les puces de lit. Je n’ai juste pas de preuves…', 3, 3, 1);
+
 
 INSERT INTO reviews(review, rating, category_id, tenancy_id)
-VALUES('I liked the local park!', 3, 2, 2);
+VALUES('Ne signez pas de bail! J’ai vécu ici 3 ans parce que le logement était beau et à un bon prix, mais après avoir déménagé j’ai réalisé que j aurais pu faire beaucoup mieux. Pas pour rien que le prix était abordable!', 1, 1, 2);
 
 INSERT INTO reviews(review, rating, category_id, tenancy_id)
-VALUES('The landlords were nice, but took a while to respond to maintenance requests', 3, 3, 1);
+VALUES('Plateau FTW! Plus, il y a une place de smoked meat juste au coin qui est malade! Vraiment un quartier à découvrir.', 5, 2, 2);
 
 INSERT INTO reviews(review, rating, category_id, tenancy_id)
-VALUES('The landlords were fantastic! We are practically family now!', 5, 3, 2);
+VALUES('Je me suis toujours bien entendu avec la proprio, et elle n était jamais sur les lieux, donc pas vraiment de raison de me plaindre.', 5, 3, 2);
+
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 2, 1, 3);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('Café, boucherie, et restaurants juste au coin. J’adore ma ville!', 5, 2, 3);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 4, 3, 3);
+
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 5, 1, 4);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('Après avoir quitté Paris pour venir vivre à Montréal, je suis venu habiter le Plateau Mont-Royal. J’adore le quartier! Je suis entouré de tous mes amis parisiens!', 5, 2, 4);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 4, 3, 5);
+
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('DO. NOT. MOVE. HERE.', 1, 1, 5);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('Best neighbourhood hands down.', 5, 2, 5);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 4, 3, 5);
+
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 1, 1, 6);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('The neighbourhood has some fantastic shops to explore! ', 5, 2, 6);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 4, 3, 6);
+
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 1, 1, 7);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('Mount-Royal is a great park for my chickens to run!', 5, 2, 7);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 4, 3, 7);
+
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 1, 1, 8);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('My AE86 looks so good parked on this street.', 5, 2, 8);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 4, 3, 8);
+
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 1, 1, 9);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('Hate it.', 5, 2, 9);
+
+INSERT INTO reviews(review, rating, category_id, tenancy_id)
+VALUES('good', 4, 3, 9);
